4c00c6494df265e626556a17c9ffdaef6d6b7f99
Hrithik Naik <hrithink456@gmail.com>
2025-04-08 06:28:41 +0530

    the final commit

--- backened/TTS.py
+++ backened/TTS.py
@@ -2,7 +2,7 @@ import time
 import os
 from elevenlabs.client import ElevenLabs
 
-client = ElevenLabs(api_key="sk_def5c7893714b0809bbe9da069fda7105b0ea2c4bcdccbc8")
+client = ElevenLabs(api_key="sk_fd1cc94945a508fb2fccb2ec31b7f74a0633c0e155249eee")
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 RECORDINGS_DIR = os.path.join(BASE_DIR, 'static', 'output')
